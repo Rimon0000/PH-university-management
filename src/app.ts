512d8f48c0fe1489744f13d75eb929bb7e4205ed
Your Name <rimonron.ad@gmail.com>
2023-12-02 10:56:07 +0600

    create index route and refector

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,9 @@
 
-import express, { Application, Request, Response } from 'express';
+import express, { Application, Request, Response, text } from 'express';
 import cors from 'cors';
-import { studentRoutes } from './app/modules/student/student.route';
-import { UserRoutes } from './app/modules/user/user.route';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import notFound from './app/middlewares/notFound';
+import router from './app/routes';
 const app: Application = express();
 
 //parsers
@@ -12,17 +11,16 @@ app.use(express.json());
 app.use(cors());
 
 //application routes
-app.use('/api/v1/students', studentRoutes)
-app.use('/api/v1/users', UserRoutes)
+app.use('/api/v1', router)
 
 
 
-
-app.get('/', (req: Request, res: Response) => {
+const test = (req: Request, res: Response) => {
   const a = 10;
-
   res.send(a);
-});
+}
+
+app.get('/', test);
 
 
 //global error Handling middleware
