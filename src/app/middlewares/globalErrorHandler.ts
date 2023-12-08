d865a9414203611b324e548564715a1a84d6a3d2
Your Name <rimonron.ad@gmail.com>
2023-12-08 20:25:18 +0600

    handle 11000 error

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -8,6 +8,7 @@ import config from '../config';
 import handleZodError from '../errors/handleZodError';
 import handleValidationError from '../errors/handleValidationError';
 import handleCastError from '../errors/handleCastError';
+import handleDuplicateError from '../errors/handleDuplicateError';
 
 
 const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) =>{
@@ -38,13 +39,19 @@ const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) =>{
       message = simplifiedError?.message;
       errorSources = simplifiedError?.errorSources
     }
+    else if(err?.code === 11000){
+      const simplifiedError = handleDuplicateError(err)
+      statusCode = simplifiedError?.statusCode;
+      message = simplifiedError?.message;
+      errorSources = simplifiedError?.errorSources
+    }
   
     //ultimate return
     return res.status(statusCode).json({
       success: false,
       message: message,
       errorSources,
-      err,
+      // err,
       stack:config.NODE_ENV === 'development'? err?.stack : null,
     })
   }
