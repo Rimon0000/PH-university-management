702dbd45380d51924f49f78dbb62c74f972257cf
Your Name <rimonron.ad@gmail.com>
2023-12-09 12:17:25 +0600

    applied pagination and field limiting

--- src/app/modules/student/student.model.ts
+++ src/app/modules/student/student.model.ts
@@ -174,7 +174,7 @@ const studentSchema = new Schema<TStudent, StudentModel>(
 
 //mongoose virtual
 studentSchema.virtual('fullName').get(function () {
-  return `${this.name.firstName} ${this.name.middleName} ${this.name.lastName}`;
+  return `${this?.name?.firstName} ${this?.name?.middleName} ${this?.name?.lastName}`;
 });
 
 
--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -27,10 +27,10 @@ const getAllStudentsFromDb = async (query: Record<string, unknown>) => {
   }))});
 
   //filtering
-  const excludeFields = ['searchTerm', 'sort', 'limit']
+  const excludeFields = ['searchTerm', 'sort', 'limit', 'page', 'fields']
 
   excludeFields.forEach((el) => delete queryObj[el])
-  // console.log({query, queryObj});
+  console.log({query}, {queryObj});
 
   const filterQuery =  searchQuery.find(queryObj)
   .populate('admissionSemester')
@@ -44,13 +44,38 @@ const getAllStudentsFromDb = async (query: Record<string, unknown>) => {
   const sortQuery = filterQuery.sort(sort)
 
   //limit
+  let page = 1
   let limit = 1
+  let skip = 0;
+
   if(query.limit){
-    limit = query.limit as number;
+    limit = Number(query.limit);
   }
 
-  const limitQuery = await sortQuery.limit(limit)
-  return limitQuery;
+  if(query.page){
+    page = Number(query.page)
+    skip = (page - 1) * limit;
+  }
+
+  const paginateQuery = sortQuery.skip(skip)
+
+  const limitQuery = paginateQuery.limit(limit)
+
+  //field limiting
+  let fields = "-__v";
+
+  /*
+  fields: 'name,email'
+  convert to
+  fields: 'name email' 
+   */
+
+  if(query.fields){
+    fields = (query.fields as string).split(",").join(" ")
+  }
+  const fieldQuery = await limitQuery.select(fields)
+
+  return fieldQuery;
 };
 
 //get single student
